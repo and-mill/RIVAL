c13239ff8140fcd54ab49ad6146ffd0a64e794d5
mueller <andreas.mueller-t1x@ruhr-uni-bochum.com>
2024-06-05 11:48:05 +0200

    added additional config for variation generation

---
+++ scripts/rival_variation_test_finnfrei.sh
@@ -0,0 +1,5 @@
+python rival/test_variation_sdv1.py \
+ --inf_config "configs/rival_variation.json" \
+ --inner_round 3 \
+ --exp_folder "out/variation_exps/finnfrei" \
+ --img_config "assets/images/configs_variation_finnfrei.json"
--- scripts/rival_variation_test_liliyth.sh
+++ scripts/rival_variation_test_liliyth.sh
@@ -1,4 +1,5 @@
-python rival/test_variation_sdv1.py --inf_config "configs/rival_variation.json" \
- --inner_round 1 \
- --exp_folder "out/variation_exps_liliyth" \
+python rival/test_variation_sdv1.py \
+ --inf_config "configs/rival_variation.json" \
+ --inner_round 3 \
+ --exp_folder "out/variation_exps/liliyth" \
  --img_config "assets/images/configs_variation_liliyth.json"
